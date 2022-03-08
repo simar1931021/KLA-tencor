e90a4e3f254d099ecf2a73608a77ac505bb2a6ce
mdsimar1901 <66200713+mdsimar1901@users.noreply.github.com>
2022-03-08 17:31:01 +0530

    Commited the working of 1B

--- milestone1B.py
+++ milestone1B.py
@@ -1,12 +1,25 @@
-import threading
-import yaml
-import datetime
+from datetime import datetime
 import time
-from threading import Thread
+import yaml
+
+import itertools
+from multiprocessing import Pool, Process
+
+
+def universal_worker(input_pair):
+    function, args = input_pair
+    return function(*args)
+
+
+def pool_args(function, *args):
+    return zip(itertools.repeat(function), zip(*args))
+
+
+
 
 def yaml_loader():
-    with open("Milestone1/Milestone1B.yaml") as file:
-        data = yaml.load(file, Loader=yaml.FullLoader)
+    with open("./Milestone1/Milestone1B.yaml") as file:
+        data = yaml.load(file,Loader = yaml.Loader)
         return data
 
 
@@ -14,47 +27,70 @@ def log_task(flow_name, task_name, msg):
     log(f'{flow_name}.{task_name} {msg}')
 
 
-def dispatch_flow(flow_name, activities):
-    for task_name in activities.keys():
-        task = activities.get(task_name)
-
-        if task.get('Type') == 'Task':
-            if task.get('Execution') == 'Sequential':
-                dispatch_sequential_tasks(flow_name, task_name, task)
-            elif task.get('Execution') == 'Concurrent':
-                dispatch_concurrent_tasks(flow_name, task_name, task)
-        if task.get('Type') == 'Flow':
-            log_flow(f'{flow_name}.{task_name}', task)
+def print_world(msg='HelloWorld', label='DEFAULT'):
+    time.sleep(2)
+    log(f'[ {label} ]: {msg}')
 
 
-def dispatch_sequential_tasks(flow_name, task_name, task):
+def dispatch_concurrently(fns):
+    proc = []
+    print(fns)
+    for fn in fns:
+        func = fn.get('function')
+        args = fn.get('args')
+        p = Process(target=func, args=args)
+        p.start()
+        proc.append(p)
+    for p in proc:
+        p.join()
+
+
+def dispatch_flow(flow_name, activities, execution='Sequential'):
+    if execution == 'Concurrent':
+        tasks_to_execute = []
+        for task_name in activities.keys():
+            task = activities.get(task_name)
+
+            function_to_call = print
+            args = []
+
+            if task.get('Type') == 'Task':
+                function_to_call = dispatch_task
+                args = [flow_name, task_name, task]
+            elif task.get('Type') == 'Flow':
+                function_to_call = dispatch_flow
+                args = [f'{flow_name}.{task_name}', task.get('Activities'), task.get('Execution')]
+
+            tasks_to_execute.append({
+                'function': function_to_call,
+                'args': args
+            })
+
+        dispatch_concurrently(tasks_to_execute)
+    elif execution == 'Sequential':
+        for task_name in activities.keys():
+            task = activities.get(task_name)
+
+            # if task.get('Type') == 'Task':
+            #   dispatch_task(flow_name, task_name, task)
+            if task.get('Type') == 'Flow':
+                dispatch_flow(f'{flow_name}.{task_name}', task.get('Activities'), task.get('Execution'))
+
+
+def dispatch_task(flow_name, task_name, task):
     log_task(flow_name, task_name, 'Entry')
     function_name = task.get('Function')
     function_args = ", ".join(list(task.get('Inputs').values()))
     execution_time = task.get('Inputs').get('ExecutionTime')
-    log_task(flow_name, task_name, f'Executing {function_name}({function_args})')
+    log_task(flow_name, task_name,  f' Executing {function_name}({function_args})')
     time.sleep(float(execution_time))
     log_task(flow_name, task_name, 'Exit')
 
 
-def dispatch_concurrent_tasks(flow_name, task_name, task):
-    log_task(flow_name, task_name, 'Entry')
-    function_name = task.get('Function')
-    function_args = ", ".join(list(task.get('Inputs').values()))
-    execution_time = task.get('Inputs').get('ExecutionTime')
-    x1 = Thread(target=log_task(flow_name, task_name, f'Executing {function_name}({function_args})'))
-    x1.start()
-    time.sleep(float(execution_time))
-    x2 = Thread(target = (log_task(flow_name, task_name, 'Exit'))
-    x2.start()
-    x1.join()
-    x2.join()
-
-
 def log(msg):
-    print(f'{datetime.now()};{msg}')
     msg = f'{datetime.now()};{msg}\n'
-    with open('res2.txt', 'a') as f:
+    print(msg)
+    with open('log.txt', 'a') as f:
         f.write(msg)
 
 
@@ -66,7 +102,7 @@ def log_flow(flow_name, flow):
         return
 
     if flow_type.lower() == 'flow':
-        dispatch_flow(flow_name, flow.get('Activities'))
+        dispatch_flow(flow_name, flow.get('Activities'), flow.get('Execution'))
 
     log(f'{flow_name} Exit')
 
@@ -78,4 +114,5 @@ def main():
         log_flow(key, pipeline[key])
 
 
-main()
+if __name__ == '__main__':
+    main()
